9a9f32590f3b662d428b3784eaec8016904bcc84
MatrixAura <1446679699@qq.com>
2023-07-08 18:33:55 +0800

    awa

--- src/main/java/cn/matrixaura/lepton/module/impl/movement/Speed.java
+++ src/main/java/cn/matrixaura/lepton/module/impl/movement/Speed.java
@@ -5,26 +5,37 @@ import cn.matrixaura.lepton.listener.events.player.EventUpdate;
 import cn.matrixaura.lepton.module.Category;
 import cn.matrixaura.lepton.module.Module;
 import cn.matrixaura.lepton.module.ModuleInfo;
+import cn.matrixaura.lepton.setting.Setting;
 import cn.matrixaura.lepton.util.player.PlayerUtils;
 import org.lwjgl.input.Keyboard;
 
 @ModuleInfo(name = "Speed", description = "Make you move faster", category = Category.Movement, key = Keyboard.KEY_G)
 public class Speed extends Module {
 
-    @Override
-    public void onEnable() {
-        mc.getTimer().setTimerSpeed(2F);
-    }
+    public Setting<Mode> mode = setting("Mode", Mode.Legit);
 
     @Listener
     public void onUpdate(EventUpdate ignored) {
-        if (mc.getPlayer().onGround() && PlayerUtils.isMoving()) {
-            mc.getPlayer().jump();
+        switch (mode.getValue()) {
+            case Legit: {
+                if (mc.getPlayer().onGround() && PlayerUtils.isMoving()) mc.getPlayer().jump();
+                break;
+            }
+            case Timer: {
+                mc.getTimer().setTimerSpeed(2F);
+                if (mc.getPlayer().onGround() && PlayerUtils.isMoving()) mc.getPlayer().jump();
+                break;
+            }
         }
     }
 
     @Override
     public void onDisable() {
-        mc.getTimer().setTimerSpeed(1F);
+        if (mode.getValue() == Mode.Timer) mc.getTimer().setTimerSpeed(1F);
+    }
+
+    public enum Mode {
+        Legit,
+        Timer
     }
 }
