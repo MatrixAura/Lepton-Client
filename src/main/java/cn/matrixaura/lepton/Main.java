6d91fd685d087f3ab0ace634337e96c31e3b40a2
MatrixAura <1446679699@qq.com>
2023-07-24 19:51:43 +0800

    未完成

--- src/main/java/cn/matrixaura/lepton/Main.java
+++ src/main/java/cn/matrixaura/lepton/Main.java
@@ -1,5 +1,7 @@
 package cn.matrixaura.lepton;
 
+import cn.matrixaura.lepton.inject.AgentMain;
+import cn.matrixaura.lepton.shellcode.impl.InjectShellcode;
 import cn.matrixaura.lepton.util.inject.InjectUtils;
 import cn.matrixaura.lepton.util.logger.ConsoleColors;
 import com.sun.tools.attach.VirtualMachine;
@@ -13,6 +15,7 @@ import java.net.URLClassLoader;
 public class Main {
 
     private static VirtualMachine vm;
+    public static InjectType injectType = InjectType.Shellcode;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
@@ -62,8 +65,18 @@ public class Main {
                 .getFile());
 
         try {
-            vm = VirtualMachine.attach(String.valueOf(pid));
-            vm.loadAgent(agentJarFile.getAbsolutePath());
+            switch (injectType) {
+                case Agent: {
+                    vm = VirtualMachine.attach(String.valueOf(pid));
+                    vm.loadAgent(agentJarFile.getAbsolutePath());
+                    break;
+                }
+                case Shellcode: {
+                    AgentMain.agentmain(null, InjectShellcode.invoke(pid));
+                    break;
+                }
+                default: throw new RuntimeException("Invalid type");
+            }
             info("Attach to " + pid);
         } catch (Exception e) {
             fail("Failed to attach");
@@ -86,5 +99,10 @@ public class Main {
 
     public static void version(String s) {
         System.out.printf("%sclient%s: Version %s%s%s\n", ConsoleColors.PURPLE, ConsoleColors.RESET, ConsoleColors.BLUE, s, ConsoleColors.RESET);
+    }
+
+    public enum InjectType {
+        Agent,
+        Shellcode
     }
 }
--- src/main/java/cn/matrixaura/lepton/shellcode/impl/InjectShellcode.java
+++ src/main/java/cn/matrixaura/lepton/shellcode/impl/InjectShellcode.java
@@ -2,5 +2,16 @@ package cn.matrixaura.lepton.shellcode.impl;
 
 import cn.matrixaura.lepton.shellcode.Shellcode;
 
+import java.lang.instrument.Instrumentation;
+
 public class InjectShellcode extends Shellcode {
+
+    public InjectShellcode() {
+        super("Inject", new byte[]{});
+    }
+
+    public static Instrumentation invoke(long pid) {
+        return null; // TODO
+    }
+
 }
