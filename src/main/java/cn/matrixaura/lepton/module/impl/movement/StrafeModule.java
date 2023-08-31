76bc5d93fcc42a4e2a060488b6116ed95ab042ba
MatrixAura <1446679699@qq.com>
2023-08-31 20:01:34 +0800

    TODO

---
+++ src/main/java/cn/matrixaura/lepton/module/impl/movement/StrafeModule.java
@@ -0,0 +1,13 @@
+package cn.matrixaura.lepton.module.impl.movement;
+
+import cn.matrixaura.lepton.listener.bus.Listener;
+import cn.matrixaura.lepton.module.Module;
+
+public class StrafeModule extends Module {
+    /*
+    @Listener
+    public void onMotion(EventMotion event) { // TODO: Until 2023/9/10 - Make player's strafe follows vanilla protocol
+
+    }
+    */
+}
